4bf735e76593aeac0c0921ce0f46dc354b08504f
Gianluca Guarini <gianluca.guarini@gmail.com>
2014-05-01 21:35:08 +0200

    closes #7

---
+++ bind.polyfill.js
@@ -0,0 +1,7 @@
+// bind polyfill by  Andrea Giammarchi https://github.com/WebReflection/micro-env/blob/master/src/Function/bind.js
+Function.bind || (Function.prototype.bind = function (c) {
+  var f = this;
+  return function () {
+    return f.apply(c, arguments);
+  };
+});
